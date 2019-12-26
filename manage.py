d3fdc71118697d3d9891f4501bceffe42a284888
natrix-fork <cqrdev@gmail.com>
2019-12-26 13:56:38 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,40 +1,47 @@
-import subprocess
-import requests
-import sys
-from urllib.parse import urlsplit
-import json
+#!/usr/bin/python
 
-error_codes = [400, 404, 403, 408, 409, 501, 502, 503]
+import whois
+from datetime import datetime
+from sys import argv, exit
+import time
 
-url = sys.argv[1]
+now = datetime.now()
 
-response = requests.get(url).text
+if len(argv) < 2:
+    print('No domain specified on the command line, usage:  ')
+    print('')
+    print('    ./check-domain.py example.net')
+    exit(1)
 
-with open('example.html', 'w') as f:
-    f.write(response)
+domain = argv[1]
+w = False
+try:
+    w = whois.whois(domain)
+except whois.parser.PywhoisError as e:
+    time.sleep(600)
+    w = whois.whois(domain)
+    exit(1)
+if w:
+    if type(w.expiration_date) == list:
+        w.expiration_date = w.expiration_date[0]
+    else:
+        w.expiration_date = w.expiration_date
 
-links = subprocess.run(['oust', 'example.html', 'links'], stdout=subprocess.PIPE).stdout.decode('utf-8')
-links = links.splitlines()
+    domain_expiration_date = str(w.expiration_date.day) + '/' + str(w.expiration_date.month) + '/' + str(
+        w.expiration_date.year)
 
-urls = []
-for link in links:
-    try:
-        status_code = requests.get(link).status_code
-    except Exception:
-        continue
-    domain = urlsplit(url).netloc
-    broken = 'Yes' if status_code in error_codes else 'No'
-    expiration = subprocess.run(['python', 'domain_exp.py', domain], stdout=subprocess.PIPE).stdout.decode('utf-8')
-    if 'ConnectionResetError' in expiration:
-        expiration = 'Unable to get data from whois.'
-    urls.append({
-        'url': link,
-        'domain': domain,
-        'broken': broken,
-        'expiration': expiration,
-        'status_code': status_code,
-    })
+    timedelta = w.expiration_date - now
+    days_to_expire = timedelta.days
 
-with open('result.json', 'w') as f:
-    json.dump(urls, f)
-
+    if 60 >= timedelta.days > 30:
+        print('WARNING: %s is going to expire in %s days, expiration date is set to %s' % (
+            domain, days_to_expire, domain_expiration_date))
+        exit(1)
+    elif timedelta.days <= 30:
+        print('WARNING: %s is going to expire in %s days, expiration date is set to %s' % (
+            domain, days_to_expire, domain_expiration_date))
+        exit(2)
+    else:
+        print('OK, the domain %s is expiring on %s, %s days to go. No need to renew at this moment of time' % (
+            domain, domain_expiration_date, days_to_expire))
+        exit(0)
